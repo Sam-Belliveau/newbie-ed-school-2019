fb12b6dc0d2693969b2f4c2bf15011e28907b351
rmui10 <33766107+rmui10@users.noreply.github.com>
2018-10-19 15:13:48 -0400

    Add parent school class

---
+++ School.java
@@ -0,0 +1,49 @@
+public class School {
+
+    static int numberOfSchools = 0;
+    int numberOfTeachers, numberOfStudents;
+
+    int population = numberOfTeachers + numberOfStudents;
+    int studentTeacherRatio = numberOfStudents / numberOfTeachers;
+
+    int numberOfClassrooms = 10;
+
+    public School(int numberOfClassrooms){
+        numberOfSchools++;
+    }
+
+    public void addStudent(){
+
+    }
+
+    public void addTeacher(){
+
+    }
+
+    public void addClassroom(){
+
+    }
+
+    private class Classroom{
+                int room;
+                int period;
+                School school;
+                Teacher teacher;
+
+                public Classroom(){
+                }
+        }
+
+     private class Person{
+        int age;
+        //When new person added/removed, increment/decrement School population by 1.
+        //Make leaveSchool() and getAge()
+    }
+
+     private class Teacher extends Person{
+     }
+
+    private class Student extends Person{
+        int grade;
+    }
+}
